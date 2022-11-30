35542f77885ea559f57a6b30318201a4259e1d40
Ricardo Manzo <ricardojuliomanzo@gmail.com>
2022-11-30 15:04:27 -0300

    segunda entrega js

--- javascript/javascriptPractica.js
+++ javascript/javascriptPractica.js
@@ -1,5 +1,5 @@
 function tienda(){ 
-iniciar = Number(prompt("                              Bienvenido a Choque Sublimables           " + 
+let iniciar = Number(prompt("                              Bienvenido a Choque Sublimables           " + 
 "                     Que producto  queres elegir?  anota el precio                  " +
 "                     1_Tazas de ceramica : $800                                              " +
 "                     2_Tazas de polimero personalizadas : $400                          " +
@@ -8,13 +8,14 @@ iniciar = Number(prompt("                              Bienvenido a Choque Subli
 
 alert("tu gasto total es $" + iniciar) ;
 
+
 function continuarComprando(){
 continuar =  Number(prompt("Que producto  queres elegir?  anota el precio" +
 "                                    1_Tazas de ceramica : $800                                              " +
 "                     2_Tazas de polimero personalizadas : $400                          " +
 "                     3_ Remeras de poliester : $1500                                         " +
 "                     4_ Almohadones: $700                       " ));
- alert("Tu gasto total es $" + (continuar + iniciar)) ;  
+ alert("Tu gasto total es $" + (iniciar + continuar)) ; 
 
 }
 comprando();
@@ -31,6 +32,28 @@ if(confirmar == true){
          }
 } 
 }
+ 
+class Producto {
+   constructor(nombre, linea, precio, talles){
+      this.nombre = nombre;
+      this.linea = linea;
+      this.precio = precio;
+      this.talles = talles;
+      }
+}
+let arrayProductos = [];
+arrayProductos.push(iniciar);
+arrayProductos.push(continuar);
+let remeraLisa = new Producto("remera Blanca Lisa", "poliester", 1500,"S, M, L, XL" );
+let remeraDignidad = new Producto("Remera  Blanca","poliester",1500, "S, M, L, XL"); 
+let remeraHomero = new Producto("Remera Blanca Homero", "poliester",1500, "S, M, L, XL");
+let almohadonDulzura = new Producto("Almohadon  50x40cm estampado","Almohadones", 1100);
+let almohadonDulzura2 = new Producto("Almohadon  50x40cm estampado","Almohadones", 1100);
+let mateListo = new Producto("Mate listo Dia del Maestro", "polimero", 800);
+let  tazaSignos = new Producto("Taza de ceramica Edicion Signos", "ceramica", 900);
+let  tazaSignos2 = new Producto("Taza de ceramica Edicion Signos", "ceramica", 900);
+let tazaSpiderman = new Producto("Taza de ceramica Edicion Superheroes", "ceramica", 9);
+
 
 
 
